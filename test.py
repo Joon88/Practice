d28de3521ea522212cae607c31aedffb1277c15f
Joon88 <joon1315088@yahoo.com>
2019-10-18 12:40:12 +0530

    office lappy

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 print('Welcome')
 print("Pushed from home lappy")
+
+def test(a):
+    if a>5:
+        x = "Hi"
+    if a>10:
+        print(x)
+        x = "Hello"
+        print(x)
+    x = "pata nahi"
+    print(x)
+
+test(100)
